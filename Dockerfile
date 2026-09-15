e5c830e08afa1a16fa2b6e9c18aecd87ebcb0a07
TonCerques <zcoltyy@gmail.com>
2026-09-14 22:01:40 -0300

    fix: não aguento mais subir pro render

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM php:8.2-apache
 
+RUN apt-get update && apt-get install -y libpq-dev \
+    && docker-php-ext-install pdo pdo_pgsql pgsql
+
 COPY . /var/www/html/
 
 RUN sed -i 's/80/${PORT}/g' /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
--- sistema/conexao.php
+++ sistema/conexao.php
@@ -1,13 +1,13 @@
 <?php
 
 $host     = getenv('DB_HOST');
-$port     = getenv('DB_PORT') ?: '6543';
+$port     = getenv('DB_PORT') ?: '5432';
 $db       = getenv('DB_NAME');
 $user     = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+    $dsn = "pgsql:host={$host};port={$port};dbname={$db};sslmode=require";
     
     $pdo = new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -15,6 +15,5 @@ try {
         PDO::ATTR_CASE               => PDO::CASE_UPPER
     ]);
 } catch (PDOException $e) {
-    die("Erro na conexão com o banco de dados.");
+    die("Erro na conexão: " . $e->getMessage());
 }
-?>
